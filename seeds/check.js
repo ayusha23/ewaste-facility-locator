64f9e8a6ed5a05a7b0be3f1b8f104578abf74d80
ayusha23 <104146592+ayusha23@users.noreply.github.com>
2023-10-23 14:50:41 +0530

    Add files via upload

---
+++ seeds/check.js
@@ -0,0 +1,43 @@
+const mongoose = require('mongoose');
+const EWPlant = require('../models/garbageplants');
+mongoose.connect("mongodb://127.0.0.1/E-Waste-Dump");
+
+const latitude = 28.6523392;
+const longitude = 77.1588096;
+const distance = 15;
+const unitValue = 1000;
+
+const nearestLoc = async () => {
+    const users = await EWPlant.aggregate([
+        {
+            $geoNear: {
+                near: {
+                    type: 'Point',
+                    coordinates: [longitude, latitude]
+                },
+                maxDistance: distance * unitValue,
+                distanceField: 'distance',
+                distanceMultiplier: 1 / unitValue,
+                key: 'Location'
+            }
+        },
+        {
+            $project: {
+                _id: 1, 
+                distance: 1
+            }
+        },
+        {
+            $sort: {
+                distance: 1
+            }
+        },
+        { $limit: 5 }
+    ]);
+    console.log(users);
+}
+
+nearestLoc();
+
+
+
